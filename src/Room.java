2d77ff5d33e87289b5185018ca7987ffb88589b4
guseklti <guseklti@student.gu.se>
2018-05-06 08:54:40 +0200

    fixed nullpointer

--- src/Room.java
+++ src/Room.java
@@ -1,38 +1,40 @@
 import java.util.ArrayList;
 
 public class Room {
-	
-    private int nr;
+
+	private int nr;
 	public int capacity;
 	public ArrayList<Patient> patientArray;
 	boolean status;
-	public Room(int capacity, ArrayList<Patient> patientArray,int nr,String status) {
+	public Room(int capacity, ArrayList<Patient> patientArray,int nr,boolean status) {
 
-		
+		this.nr = nr;
+		this.status = status;
 		this.capacity = capacity;
 		this.patientArray = patientArray;
 	}
-	
+
 	public Room(int number, int capacity2, boolean status) {
 		this.nr = number;
 		this.capacity = capacity2;
 		this.status = status;
-}
+		this.patientArray = new ArrayList<Patient>();
+	}
 	public int getCapacity() {
 		return capacity;
 	}
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-	
-	
+
+
 	public void accomodatePatient(Patient newPatient) {
 		patientArray.add(newPatient);
 	}
-	
+
 	public void removePatient(Patient patient) {
 		patientArray.remove(patient);
-			
+
 	}
 
 	public int getNr() {
@@ -59,6 +61,6 @@ public class Room {
 	public boolean isSingleRoom() {
 		return capacity == 1;
 	}
-	
+
 
 }
